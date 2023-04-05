43e5f3aa822e7f417a04b568301c9a6bf349816f
NelsonMALGAS <zmalgas69@gmail.com>
2023-04-05 08:45:09 +0200

    modified js file

--- src/Challenge3/script3.js
+++ src/Challenge3/script3.js
@@ -12,9 +12,9 @@ const divider = '----------------------------------'
 
 //-------------CONCATENATION--------------
 const owed = parseFloat(-1* leoBalance) + parseFloat(-1 *sarahBalance) 
-const leo =`${leoName} ${leoSurname}(Owed :R ${parseFloat(-1 * leoBalance).toFixed(2)})\n`
-const sarah =`${sarahName}${sarahSurname}(Owed :R ${parseFloat(-1 * sarahBalance).toFixed(2)})\n` 
-const total = `\nTotal amount owed:R${(owed).toFixed(2)}`
+const leo =`${leoName} ${(leoSurname).trim()} (Owed :R ${parseFloat(-1 * leoBalance).toFixed(2)})\n`
+const sarah =`${(sarahName).trim()} ${sarahSurname} (Owed :R ${parseFloat(-1 * sarahBalance).toFixed(2)})\n` 
+const total = `\nTotal amount owed:R ${(owed).toFixed(2)}`
 const result = leo + sarah + divider + divider + total   + '\n----------------------------------' + '----------------------------------'
 
 
@@ -24,6 +24,7 @@ console.log(result)
 //CONCATENATION -> is the process of combining two or more strings
 //into a single string. In JavaScript, you can use the concatenation operator
 //`+` to concatenate strings.
+//.trim eliminates access spaces
 
 
 
